0d1277a31fab5f1b2d99fdc91942bf961a7e42af
karan2399 <karan2399@gmail.com>
2024-06-20 11:01:40 -0400

    time on home

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,16 +15,45 @@ export class Tab1Page implements OnInit{
   currentDay!: string;
   currentDateFormatted!: string;
   amPm!: string;
+  currentTime!: string;
+  greeting!: string;
   constructor(public ocrService:OcrService) {
 
   }
 
   ngOnInit(){
-    this.currentDate = new Date();
-    this.amPm =   this.currentDate.getHours() >= 12 ? ' PM' : ' AM';
-    this.currentDateFormatted = this.currentDate.getFullYear() + "-" + (this.currentDate.getMonth() + 1) + "-" + this.currentDate.getDate() + " " +  this.currentDate.getHours() + ":" + this.currentDate.getMinutes() + this.amPm;
-    this.currentDay = this.currentDate.toLocaleDateString('en-US', { weekday: 'long' });        
+    this.updateTime();
+  
   }
+
+   updateTime = () => {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const strTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
+
+    let greeting;
+    if (now.getHours() < 12) {
+        greeting = 'Good Morning';
+    } else if (now.getHours() < 18) {
+        greeting = 'Good Afternoon';
+    } else if (now.getHours() < 22) {
+        greeting = 'Good Evening';
+    } else {
+        greeting = 'Good Night';
+    }
+
+    this.currentTime = strTime;
+    this.greeting = greeting;
+    setInterval(this.updateTime, 1000);
+}
+
+
   
 
 }
